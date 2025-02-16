4f095fbbc0513ab9631fd09535e81070f99cecfb
Yusuf Babatunde <Yusuf.itexpert@gmail.com>
2025-02-16 01:26:36 -0800

    ui corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { Suspense } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import SearchBar from "./components/search-bar";
 import PhotoGrid from "./components/photo-grid";
-import PhotoGridSkeleton from "./components/photo-grid-skeleton";
-
 
 export default function Home({
   searchParams,
@@ -33,9 +30,7 @@ export default function Home({
         </Container>
       </Box>
       <Container maxWidth="lg" sx={{ mt: { xs: 4, md: 6 } }}>
-        <Suspense fallback={<PhotoGridSkeleton />}>
-          <PhotoGrid searchParams={searchParams} />
-        </Suspense>
+        <PhotoGrid searchParams={searchParams} />
       </Container>
     </main>
   );
